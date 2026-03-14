762b7bbfb67d1a56696c50ab347a683a324b53d3
kriztinajg-jpg <kriztinajg@gmail>
2026-03-14 14:13:10 -0500

    creacion clase abstract en person

--- src/main/java/co/edu/cesdega/app/Main.java
+++ src/main/java/co/edu/cesdega/app/Main.java
@@ -3,8 +3,13 @@ import co.edu.cesdega.models.Person;
 import co.edu.cesdega.models.Student;
 import co.edu.cesdega.models.Teacher;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+public static void main(String[] args) {
+    Teacher teacher = new Teacher(1L, null, "CC", "1122334455", "Alice", "Johnson", "ACTIVE");
+
+    Student student = new Student(null, "TI", "6677889900", "Charlie", "Brown", "ACTIVE", "2000-01-01");
+    student.setStudentId(1L);
+
+    System.out.println("=== DEMO FASE 1 ===");
+    System.out.println(teacher);
+    System.out.println(student);
 }
